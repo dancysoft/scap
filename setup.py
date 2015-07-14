15019a853d0751497796f7bc3c43e5e77cda61b5
Mukunda Modell <mmodell@wikimedia.org>
2015-07-14 16:36:54 -0500

    setup tools support
    Change-Id: Idbbb8427c256a9c5b76bb47971464e662111f741

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+long_description = open(
+    os.path.join(
+        os.path.dirname(__file__),
+        'README.rst'
+    )
+).read()
+
+setup(name='Scap',
+      version='1.0',
+      description='Wikimedia Deployment Tool',
+      long_description=long_description,
+      author='Wikimedia Foundation Contributors',
+      author_email='bd808@wikimedia.org',
+      url='https://doc.wikimedia.org/mw-tools-scap/',
+      packages=find_packages('.'),
+      entry_points={
+        'console_scripts': [
+            'scap = scap.scap',
+        ]
+      },
+      install_requires=[
+        "psutil",
+        "netifaces",
+        'prompt-toolkit==0.41',
+      ],
+     )
